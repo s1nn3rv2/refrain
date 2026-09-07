c359ccdbd67a2a9faecd2e3bc438564604863c6b
s1nn3rv2 <62899437+s1nn3rv2@users.noreply.github.com>
2026-09-07 22:11:30 +0200

    refactor: extracted cache_path to a util function

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,10 @@
+use std::{
+    env::home_dir,
+    fs,
+    path::{Path, PathBuf},
+    time::UNIX_EPOCH,
+};
+
 pub trait DurationExt {
     fn format_time(&self) -> String;
 }
@@ -26,4 +33,30 @@ pub fn fnv1a(bytes: &[u8]) -> u64 {
         hash = hash.wrapping_mul(0x0000_0100_0000_01B3);
     }
     hash
+}
+
+/// Returns modified time of a file at path, returns 0 if cannot get
+pub fn get_mtime(file_path: &Path) -> u128 {
+    fs::metadata(file_path)
+        .ok()
+        .and_then(|m| m.modified().ok())
+        .and_then(|t| t.duration_since(UNIX_EPOCH).ok())
+        .map(|d| d.as_nanos())
+        .unwrap_or(0)
+}
+
+/// Utility to return cache path
+/// `~/.cache/refrain/<dir>/<hash>.<ext>`
+/// Keyed by path and modified time
+pub fn cache_path(track_path: &Path, dir: &str, ext: &str) -> PathBuf {
+    let mtime = get_mtime(track_path);
+
+    let key = format!("{}:{mtime}", track_path.display());
+    let hash = fnv1a(key.as_bytes());
+
+    home_dir()
+        .unwrap_or_else(|| PathBuf::from("."))
+        .join(".cache/refrain")
+        .join(dir)
+        .join(format!("{hash:016x}.{ext}"))
 }
--- src/waveform.rs
+++ src/waveform.rs
@@ -1,14 +1,12 @@
 use std::{
-    env::home_dir,
     fs::{self, File},
     io::{Read, Write},
-    path::{Path, PathBuf},
-    time::UNIX_EPOCH,
+    path::Path,
 };
 
 use audio_waveform::{ChannelMode, Measure, WaveformOptions};
 
-use crate::util::fnv1a;
+use crate::util;
 
 pub const WAVEFORM_POINTS: usize = 512;
 // we use this instead of Vec<u8> as we know exactly how many points we have in each waveform
@@ -16,30 +14,9 @@ pub const WAVEFORM_POINTS: usize = 512;
 // f32 to u8 from audio_waveform crate, could change it in future if needed
 pub type WaveformData = [u8; WAVEFORM_POINTS];
 
-/// Return cache path of a track, keyed by path and modified time
-/// Example path: ~/.cache/refrain/waveforms/<hash>.bin
-pub fn cache_path(track_path: &Path) -> PathBuf {
-    // get modified time, returns 0 if cannot get
-    let mtime = fs::metadata(track_path)
-        .ok()
-        .and_then(|m| m.modified().ok())
-        .and_then(|t| t.duration_since(UNIX_EPOCH).ok())
-        .map(|d| d.as_nanos())
-        .unwrap_or(0);
-
-    let key = format!("{}:{mtime}", track_path.display());
-    let hash = fnv1a(key.as_bytes());
-
-    let file_name = format!("{hash:016x}.bin");
-    home_dir()
-        .unwrap_or_else(|| PathBuf::from("."))
-        .join(".cache/refrain/waveforms")
-        .join(file_name)
-}
-
 /// Decode file to peaks, or read from disk cache
 pub fn load_or_compute(track_path: &Path) -> Option<WaveformData> {
-    let file = cache_path(track_path);
+    let file = util::cache_path(track_path, "waveforms", "bin");
 
     // check for disk cache
     if let Ok(mut f) = File::open(&file) {
